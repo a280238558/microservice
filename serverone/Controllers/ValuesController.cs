7a77d8c718a61ec5593134b0c640b416554ea674
Wanghao <Wanghao@DESKTOP-OGFI19I>
2017-10-20 12:28:10 +0800

    添加项目文件。

---
+++ serverone/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pivotal.Discovery.Client;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+
+namespace serverone.Controllers
+{
+    [Route("/")]
+    public class ValuesController : Controller
+    {
+      
+
+
+        [HttpGet]
+        public string Get()
+        {
+            return "order1";
+          
+        }
+
+    }
+}
